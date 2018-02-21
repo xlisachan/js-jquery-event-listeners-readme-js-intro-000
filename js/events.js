21f0a50aa3c4b3c317988630cb3fc378bd586e74
Lisa Chan <xlisachan@gmail.com>
2018-02-21 16:03:40 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 
 function pressIt() {
   $('#typing').on('keydown', function(key) {
-    if (key.which == 71){
+    if (key.which == 71) {
       alert('G was pressed');
     }
   });
